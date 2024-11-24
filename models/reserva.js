b6cd35b3e4da94e83a0a3c9548133c5c8e331549
Iris Zamora <iris.z@hotmail.com>
2024-11-24 02:00:32 -0300

    creamos el proyecto

---
+++ models/reserva.js
@@ -0,0 +1,19 @@
+
+import mongoose from 'mongoose';
+
+const reservaSchema = mongoose.Schema({
+  cliente: { type: String, required: true },
+  email: { type: String, required: true },
+  fecha: { type: String, required: true },
+  hora: { type: String, required: true },
+  numeroPersonas:{ type: Number, required: true },
+  mesa: { type: Number, required: true },
+  estado: { type: String, default: 'Activa' },
+  reservadoPor: { type: String, default: 'Cliente' },
+}, { timestamps: true });
+
+const Reserva = mongoose.model('Reserva', reservaSchema);
+
+export default Reserva;
+
+  
