719767ffeb044e6028947db773c067638f9ea9ab
Iris Zamora <iris.z@hotmail.com>
2024-11-24 02:40:34 -0300

    Modificacion para desplegar en vercel

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import { Server } from 'socket.io';
 import moment from 'moment';
 import connectDB from './db.js';
 import dotenv from 'dotenv';
+import serverless from 'serverless-http';
 
 // Cargar las variables de entorno desde el archivo .env
 dotenv.config();
@@ -151,3 +152,5 @@ server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
+
+export default serverless(app);
