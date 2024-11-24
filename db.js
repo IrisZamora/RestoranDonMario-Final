75841c0807ce079079e34cd6fe21ea6e3545b4af
Iris Zamora <iris.z@hotmail.com>
2024-11-24 02:26:33 -0300

    Modificar

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI); 
+    const conn = await mongoose.connect("mongodb+srv://iriszamora1525:VcSXkDvkzQF8yEVJ@cluster0.nbm3i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"); 
     console.log(`MongoDB conectado: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
